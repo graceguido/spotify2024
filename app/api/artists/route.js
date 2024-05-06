76050a5fa2c9f8a097031f9c70aed6bf78a55e03
Grace Guido <graceguido@Graces-MacBook-Pro.local>
2024-05-06 14:18:40 -0400

    spotify API

---
+++ app/api/artists/route.js
@@ -0,0 +1,40 @@
+import { getAccessToken } from "../../../lib/spotify";
+
+export async function GET() {
+  const token = await getAccessToken();
+
+  const artists = [
+    {
+      name: "Taylor Swift",
+      spotify_id: "06HL4z0CvFAxyc27GXpf02",
+    },
+    {
+      name: "Beyoncé",
+      spotify_id: "6vWDO969PvNqNYHIOW5v0m",
+    },
+    {
+      name: "Ariana Grande",
+      spotify_id: "66CXWjxzNUsdJxJ2JdwvnR",
+    },
+    {
+      name: "Kendrick Lamar",
+      spotify_id: "2YZyLoL8N0Wb9xBt1NhZWg",
+    },
+  ];
+
+  const artistsIds = artists.map((artist) => artist.spotify_id);
+  const artistsString = artistsIds.join(",");
+
+  const apiResponse = await fetch(
+    `https://api.spotify.com/v1/artists?ids=${artistsString}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const artistData = await apiResponse.json();
+
+  return new Response(JSON.stringify(artistData));
+}
