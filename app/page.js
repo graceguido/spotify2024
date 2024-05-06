cc8ea2f3b2e73f299727a3da89394e9eb0f96137
Grace Guido <graceguido@Graces-MacBook-Pro.local>
2024-05-06 13:34:28 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,53 @@
+import Heading from "../components/Heading";
+import Paragraph from "../components/Paragraph";
+
+const Home = () => {
+  return (
+    <main>
+      <Heading level={1} marginLeft={6}>
+        Spotify
+      </Heading>
+      <Heading level={2} marginBottom={4}>
+        Artists
+      </Heading>
+      <Heading level={3}>Songs</Heading>
+      <Paragraph intro marginBottom={12}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Paragraph>
+      <Paragraph marginBottom={2}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Paragraph>
+      <Paragraph marginBottom={5}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Paragraph>
+      <Paragraph caption>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Paragraph>
+    </main>
+  );
+};
+export default Home;
---
+++ components/Paragraph/index.js
@@ -0,0 +1,26 @@
+import classnames from "classnames/bind";
+import styles from "./Paragraph.module.scss";
+
+const cx = classnames.bind(styles);
+
+const Paragraph = ({
+  caption,
+  children,
+  intro,
+  marginTop,
+  marginRight,
+  marginBottom,
+  marginLeft,
+}) => {
+  const paragraphClasses = cx({
+    paragraph: true,
+    intro: intro,
+    caption: caption,
+    [`margin-top--${marginTop}`]: marginTop,
+    [`margin-right--${marginRight}`]: marginRight,
+    [`margin-bottom--${marginBottom}`]: marginBottom,
+    [`margin-left--${marginLeft}`]: marginLeft,
+  });
+  return <p className={paragraphClasses}>{children}</p>;
+};
+export default Paragraph;
