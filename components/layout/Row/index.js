87202442f4b3b9bcedb78f378c2990ad8f92eeab
Grace Guido <graceguido@graces-mbp.mynetworksettings.com>
2024-05-07 00:40:36 -0400

    motion

--- components/layout/Row/index.js
+++ components/layout/Row/index.js
@@ -7,6 +7,7 @@ const cx = classnames.bind(styles);
 const Row = ({
   alignItems,
   borderBottom,
+  borderTop,
   children,
   justifyContent,
   paddingBottom,
@@ -17,6 +18,7 @@ const Row = ({
     [`justify-content-${justifyContent}`]: justifyContent,
     [`align-items-${alignItems}`]: alignItems,
     [`border-bottom-${borderBottom}`]: borderBottom,
+    [`border-top-${borderTop}`]: borderTop,
     [`padding-top-${paddingTop}`]: paddingTop,
     [`padding-bottom-${paddingBottom}`]: paddingBottom,
   });
